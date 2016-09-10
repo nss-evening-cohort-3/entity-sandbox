dc152363c74d072a93296615b9fdff27b2ae0135
Jurnell Cockhren <jurnell@sophicware.com>
2016-09-10 10:03:35 -0700

    Added BlogRepository class with basic Tests

---
+++ ExploreEntity.Tests/DAL/BlogRepositoryTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExploreEntity.DAL;
+using System.Collections.Generic;
+using ExploreEntity.Models;
+
+namespace ExploreEntity.Tests.DAL
+{
+    [TestClass]
+    public class BlogRepositoryTests
+    {
+        [TestMethod]
+        public void RepoEnsureCanCreateInstance()
+        {
+            BlogRepository repo = new BlogRepository();
+            Assert.IsNotNull(repo);
+        }
+
+        [TestMethod]
+        public void RepoEnsureWeHaveNoAuthors()
+        {
+            // Arrange
+            BlogRepository repo = new BlogRepository();
+
+            // Act
+            List<Author> actual_authors = repo.GetAuthors();
+            int expected_authors_count = 0;
+            int actual_authors_count = actual_authors.Count;
+
+            // Assert
+            Assert.AreEqual(expected_authors_count, actual_authors_count);
+        }
+    }
+}
---
+++ ExploreEntity/DAL/BlogRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ExploreEntity.Models;
+
+namespace ExploreEntity.DAL
+{
+    public class BlogRepository
+    {
+        public List<Author> GetAuthors()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
