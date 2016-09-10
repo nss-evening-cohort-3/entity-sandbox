11f09cb2e7cc909bbf8a9cbf335771c0aa60b205
Jurnell Cockhren <jurnell@sophicware.com>
2016-09-10 08:35:22 -0700

    Add relations between PublishedWork -> CitationStyle and PublishedWork -> Author. Ensure Title, DatePublished and Citation properties are marked as NOT NULL when generating PublishedWorks table.

--- ExploreEntity/DAL/BlogContext.cs
+++ ExploreEntity/DAL/BlogContext.cs
@@ -9,6 +9,7 @@ namespace ExploreEntity.DAL
 {
     public class BlogContext : DbContext
     {
-        
+        public DbSet<PublishedWork> Works { get; set; }
+        public DbSet<CitationStyle> CitationStyles { get; set; } // To make seeding easy
     }
 }
--- ExploreEntity/Models/PublishedWork.cs
+++ ExploreEntity/Models/PublishedWork.cs
@@ -10,7 +10,17 @@ namespace ExploreEntity.Models
     {
         [Key]
         public int PublishedWorkId { get; set; }
+
+        [Required]
         public string Title { get; set; }
+
+        [Required]
         public DateTime DatePublished { get; set; }
+
+        [Required]
+        public CitationStyle Citation { get; set; } // Navigation Property
+
+
+        public List<Author> Authors { get; set; }
     }
 }
