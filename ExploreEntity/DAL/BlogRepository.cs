c414844bf1c494155117575d9c135164f625301e
Jurnell Cockhren <jurnell@sophicware.com>
2016-09-14 17:53:28 -0700

    Attempted to implement method

--- ExploreEntity/DAL/BlogRepository.cs
+++ ExploreEntity/DAL/BlogRepository.cs
@@ -22,12 +22,14 @@ namespace ExploreEntity.DAL
 
         public List<Author> GetAuthors()
         {
+            int i = 1;
             return Context.Authors.ToList();
         }
 
         public void AddAuthor(Author author)
         {
-            throw new NotImplementedException();
+            Context.Authors.Add(author);
+            Context.SaveChanges();
         }
     }
 }
