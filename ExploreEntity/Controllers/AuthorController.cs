8a1f7152a61da3a7b89110376a734593b23ff1a6
Jurnell Cockhren <jurnell@sophicware.com>
2016-09-17 11:35:25 -0700

    Created Example Controller using Repository by hand

---
+++ ExploreEntity/Controllers/AuthorController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ExploreEntity.DAL;
+using ExploreEntity.Models;
+
+namespace ExploreEntity.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private BlogRepository repo = new BlogRepository();
+
+        public ActionResult Index()
+        {
+            List<Author> list_of_authors = repo.GetAuthors();
+            return View();
+        }
+
+        public ActionResult PenUsed(string penname)
+        {
+            return View();
+        }
+    }
+}
